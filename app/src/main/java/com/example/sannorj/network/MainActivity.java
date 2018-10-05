81387b4b59351f63b48a5555a4296882bb29a4ca
kishanthbelic <kishanthprab@gmail.com>
2018-10-05 15:17:29 +0530

    notification

--- app/src/main/java/com/example/sannorj/network/MainActivity.java
+++ app/src/main/java/com/example/sannorj/network/MainActivity.java
@@ -1,10 +1,22 @@
 package com.example.sannorj.network;
 
+import android.Manifest;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.RecoverySystem;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -12,6 +24,7 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
@@ -19,9 +32,12 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.sannorj.network.PushNotification.FirebaseIDService;
-import com.example.sannorj.network.PushNotification.MyFirebaseMessagingService;
+
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.GeofencingClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.model.LatLng;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -30,6 +46,8 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -53,6 +71,8 @@ public class MainActivity extends AppCompatActivity
     String currentUserID;
 
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -71,13 +91,7 @@ public class MainActivity extends AppCompatActivity
         getSupportActionBar().setTitle("Home");
 
 
-        //aadasadasdadadasd
-        FirebaseIDService FireIdService = new FirebaseIDService();
-        FireIdService.onTokenRefresh();
 
-        MyFirebaseMessagingService myFireMsg = new MyFirebaseMessagingService();
-
-        
 
         AddNewPostButton = (ImageButton) findViewById(R.id.add_new_post_button);
         Chatbutton = (ImageButton) findViewById(R.id.chatbot);
@@ -102,6 +116,10 @@ public class MainActivity extends AppCompatActivity
         View navView = navigationView.inflateHeaderView(R.layout.navigation_header);
         NavProfileImage = (CircleImageView) navView.findViewById(R.id.nav_profile_image);
         NavProfileUserName = (TextView) navView.findViewById(R.id.nav_user_full_name);
+
+
+
+
 
 
         UsersRef.child(currentUserID).addValueEventListener(new ValueEventListener() {
@@ -148,7 +166,10 @@ public class MainActivity extends AppCompatActivity
             @Override
             public void onClick(View v)
             {
+
                 SendUserToPostActivity();
+
+
             }
         });
 
@@ -198,6 +219,8 @@ public class MainActivity extends AppCompatActivity
                 };
         postList.setAdapter(firebaseRecyclerAdapter);
     }
+
+
 
 
 
--- app/src/main/java/com/example/sannorj/network/MapsActivity.java
+++ app/src/main/java/com/example/sannorj/network/MapsActivity.java
@@ -1,11 +1,29 @@
 package com.example.sannorj.network;
 
+import android.Manifest;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.GeofencingClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -30,13 +48,47 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     private GoogleMap mMap;
 
+
+
     FirebaseDatabase database;
     DatabaseReference ref,PostRef;
     //authendication
     private FirebaseAuth mAuth;
     Double lati,longi;
     String cat;
-    ArrayList<Double> latlongList;
+    ArrayList<LatLng> latlongList;
+
+
+    private static final int PENDINGINTENT_REQUEST_CODE = 6;
+
+    //geoFence declarations
+    private static final int REQUEST_PERMISSIONS_GEOFENCE_REQUEST_CODE = 5;
+    private GeofencingClient mGeofencingClient;
+    private ArrayList<Geofence> mGeofenceList;
+    private PendingIntent mGeofencePendingIntent;
+
+
+    LocationManager locMan;
+    LocationListener locLin;
+    LatLng currentLocation;
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED&&requestCode ==3){
+
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
+            {
+
+                locMan.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,locLin);
+            }
+
+
+        }
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,8 +99,96 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-        latlongList = new ArrayList<>();
-        latlongList.clear();
+        latlongList = new ArrayList<LatLng>();
+
+
+
+        Intent intent = new Intent(getApplicationContext(),MapsActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(),12,intent,PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationManager notiMan = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+
+            notiMan.createNotificationChannel(new NotificationChannel("12","channel_Name",NotificationManager.IMPORTANCE_LOW));
+
+        }
+
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(getApplicationContext());
+        notificationBuilder
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setContentTitle("asdas")
+                .setContentText("asdas")
+                .setContentIntent(pendingIntent)
+                .setDefaults(Notification.DEFAULT_LIGHTS | Notification.DEFAULT_VIBRATE | Notification.DEFAULT_SOUND)
+                .setAutoCancel(false);
+
+
+
+        notiMan.notify((int)(System.currentTimeMillis()/1000),notificationBuilder.build());
+
+
+
+        //createNotification("asdasd","asdads");
+
+
+        //location
+        locMan = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+        locLin = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+
+                currentLocation = new LatLng(location.getLatitude(),location.getLongitude());
+                Log.i("GeoFence", "current location : "+ currentLocation.toString());
+
+
+                float[] results =new float[10];
+                if (!latlongList.isEmpty()) {
+                    LatLng lataa =latlongList.get(0);
+                    Location.distanceBetween(currentLocation.latitude, currentLocation.longitude,lataa.latitude , lataa.longitude, results);
+                    Log.i("location distance :", "" + String.valueOf(results[0]));
+
+                    try{
+                        createNotification("GEO Fence","message");
+                    }
+                    catch (Exception e){
+                        Log.i("location distance :", "aahaan");
+                    }
+
+
+                }
+                //createGeofence(location.);
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+
+            }
+        };
+
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 3);
+        } else {
+
+            locMan.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locLin);
+        }
+
+
+
+
 
 
         database = FirebaseDatabase.getInstance();
@@ -65,7 +205,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
                 Log.i("datasnapshot",""+dataSnapshot.getKey() + " " +dataSnapshot.getChildren() + dataSnapshot.hasChildren());
 
-
+                latlongList.clear();
                   for (DataSnapshot child : dataSnapshot.getChildren()) {
                       Log.i("datasnap Post name :", child.getKey() + " ");
                       Log.i("datasnap c values :",child.child("latitude").getValue()+" "+child.child("longitude").getValue());
@@ -74,6 +214,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
                           lati = (Double) child.child("latitude").getValue();
                           longi = (Double) child.child("longitude").getValue();
+
+                          latlongList.add(new LatLng(lati,longi));
+
 
                           cat = (String)child.child("type").getValue();
                           System.out.println("food is"+cat);
@@ -121,6 +264,28 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
  //  mMap.addMarker(new MarkerOptions().position(new LatLng(lati,longi)).title("Food Issues").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)));
 
 
+
+
+
+
+    // Create notification
+    public Notification createNotification(String title,String msg) {
+
+        Intent intent = new Intent(this,MapsActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this,PENDINGINTENT_REQUEST_CODE,intent,PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this);
+        notificationBuilder
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setColor(Color.RED)
+                .setContentTitle(title)
+                .setContentText(msg)
+                .setContentIntent(pendingIntent)
+                .setDefaults(Notification.DEFAULT_LIGHTS | Notification.DEFAULT_VIBRATE | Notification.DEFAULT_SOUND)
+                .setAutoCancel(false);
+
+        return notificationBuilder.build();
+    }
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
@@ -147,4 +312,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
 
     }
+
+
+
+
+
+
 }
